c5d4820312fd438d39d770f6fa5c5d054b37c3fa
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-12 20:02:20 +0300

    Задача 2

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b:");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
